480ab0c21d18384bfe79a62cf36e94793fe533f1
Ryan Greenlee <ryan.greenlee1@gmail.com>
2017-04-24 15:51:59 -0400

    Worked on DatabaseConnector. -Ryan

--- src/com/company/DatabaseConnector.java
+++ src/com/company/DatabaseConnector.java
@@ -1,12 +1,23 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by iassona on 4/21/2017.
  */
 public class DatabaseConnector {
 
-    public DatabaseConnector() {
-
+    public static void main(String[] args) {
+    	String connectionUrl = "jdbc:sqlserver://localhost:1433;" + 
+    			"databaseName=GradeReport;user=MyUserName;password=*****;";
+    	try {
+			Connection con = DriverManager.getConnection(connectionUrl);
+		} catch (SQLException exception) {
+			// TODO Auto-generated catch-block stub.
+			exception.printStackTrace();
+		}
     }
 
     public boolean authenticateLogin(String username, String password) {
