1cbd54c34cb2cfd739e1545415ae8016411309d3
Ryan Greenlee <ryan.greenlee1@gmail.com>
2017-04-30 13:45:09 -0400

    Got overall grades to show

--- src/com/company/GRFrame.java
+++ src/com/company/GRFrame.java
@@ -91,6 +91,60 @@ public class GRFrame extends JFrame{
 					exception.printStackTrace();
 				}				
 		}});
+        
+        menu.add(menuItem);
+        
+        menuItem = new JMenuItem("Show student grade roster");
+        menuItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub.
+				System.out.println("menu pressed User: " + userID);
+				
+				JTable table = new JTable();
+				DefaultTableModel model = new DefaultTableModel();
+				table.setModel(model);
+				table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+				//table.setFillsViewportHeight(true);
+				JScrollPane scroll = new JScrollPane(table);
+				scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+				
+				try {
+				Connection con = DatabaseConnector.getConnection();
+				String SQL = "EXEC studentGradeRoster " + userID;
+				
+				PreparedStatement pstmt = con.prepareStatement(SQL);
+				ResultSet rs = pstmt.executeQuery();
+				
+				ResultSetMetaData meta = rs.getMetaData();
+				int columnCount = meta.getColumnCount();
+				
+				String[] columnNames = new String[columnCount];
+				
+				for (int i = 0; i < columnCount; i++) {
+					columnNames[i] = meta.getColumnName(i + 1);
+				}
+				
+				model.setColumnIdentifiers(columnNames);
+				
+				while (rs.next()) {
+					Object[] data = new Object[columnCount];
+					for (int i = 0; i < columnCount; i++) {
+						data[i] = rs.getString(columnNames[i]);
+					}
+					model.addRow(data);
+				}
+				add(scroll);
+				setVisible(true);
+				
+				} catch (SQLException exception) {
+					// TODO Auto-generated catch-block stub.
+					exception.printStackTrace();
+				}				
+		}});
+        
+        
         menu.add(menuItem);
         this.setJMenuBar(menuBar);
         //setVisible(true);
