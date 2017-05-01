86c30f5a9b3c81f84ca23a2e249f152e77308991
Ryan Greenlee <ryan.greenlee1@gmail.com>
2017-05-01 08:46:20 -0400

    Made table with function

--- src/com/company/GRFrame.java
+++ src/com/company/GRFrame.java
@@ -51,14 +51,6 @@ public class GRFrame extends JFrame{
 				System.out.println("menu pressed User: " + userID);
 				getContentPane().removeAll();
 				
-				JTable table = new JTable();
-				DefaultTableModel model = new DefaultTableModel();
-				table.setModel(model);
-				table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-				//table.setFillsViewportHeight(true);
-				JScrollPane scroll = new JScrollPane(table);
-				scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-				
 				try {
 				Connection con = DatabaseConnector.getConnection();
 				String SQL = "EXEC studentAssignments " + userID;
@@ -66,26 +58,7 @@ public class GRFrame extends JFrame{
 				PreparedStatement pstmt = con.prepareStatement(SQL);
 				ResultSet rs = pstmt.executeQuery();
 				
-				ResultSetMetaData meta = rs.getMetaData();
-				int columnCount = meta.getColumnCount();
-				
-				String[] columnNames = new String[columnCount];
-				
-				for (int i = 0; i < columnCount; i++) {
-					columnNames[i] = meta.getColumnName(i + 1);
-				}
-				
-				model.setColumnIdentifiers(columnNames);
-				
-				while (rs.next()) {
-					Object[] data = new Object[columnCount];
-					for (int i = 0; i < columnCount; i++) {
-						data[i] = rs.getString(columnNames[i]);
-					}
-					model.addRow(data);
-				}
-				add(scroll);
-				setVisible(true);
+				makeTable(rs);
 				
 				} catch (SQLException exception) {
 					// TODO Auto-generated catch-block stub.
@@ -104,14 +77,6 @@ public class GRFrame extends JFrame{
 				System.out.println("menu pressed User: " + userID);
 				getContentPane().removeAll();
 				
-				JTable table = new JTable();
-				DefaultTableModel model = new DefaultTableModel();
-				table.setModel(model);
-				table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-				//table.setFillsViewportHeight(true);
-				JScrollPane scroll = new JScrollPane(table);
-				scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-				
 				try {
 				Connection con = DatabaseConnector.getConnection();
 				String SQL = "EXEC studentGradeRoster " + userID;
@@ -119,27 +84,7 @@ public class GRFrame extends JFrame{
 				PreparedStatement pstmt = con.prepareStatement(SQL);
 				ResultSet rs = pstmt.executeQuery();
 				
-				ResultSetMetaData meta = rs.getMetaData();
-				int columnCount = meta.getColumnCount();
-				
-				String[] columnNames = new String[columnCount];
-				
-				for (int i = 0; i < columnCount; i++) {
-					columnNames[i] = meta.getColumnName(i + 1);
-				}
-				
-				model.setColumnIdentifiers(columnNames);
-				
-				while (rs.next()) {
-					Object[] data = new Object[columnCount];
-					for (int i = 0; i < columnCount; i++) {
-						data[i] = rs.getString(columnNames[i]);
-					}
-					model.addRow(data);
-				}
-				add(scroll);
-				setVisible(true);
-				
+				makeTable(rs);
 				} catch (SQLException exception) {
 					// TODO Auto-generated catch-block stub.
 					exception.printStackTrace();
@@ -156,6 +101,42 @@ public class GRFrame extends JFrame{
 
     public void setUser(int userID) {
     	this.userID = userID;
+    }
+    
+    public void makeTable(ResultSet rs) {
+    	JTable table = new JTable();
+		DefaultTableModel model = new DefaultTableModel();
+		table.setModel(model);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		//table.setFillsViewportHeight(true);
+		JScrollPane scroll = new JScrollPane(table);
+		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+		try {
+		ResultSetMetaData meta = rs.getMetaData();
+		int columnCount = meta.getColumnCount();
+		
+		String[] columnNames = new String[columnCount];
+		
+		for (int i = 0; i < columnCount; i++) {
+			columnNames[i] = meta.getColumnName(i + 1);
+		}
+		
+		model.setColumnIdentifiers(columnNames);
+		
+		while (rs.next()) {
+			Object[] data = new Object[columnCount];
+			for (int i = 0; i < columnCount; i++) {
+				data[i] = rs.getString(columnNames[i]);
+			}
+			model.addRow(data);
+		}
+		add(scroll);
+		setVisible(true);
+		} catch (SQLException exception) {
+			// TODO Auto-generated catch-block stub.
+			exception.printStackTrace();
+		}
     }
 
 }
